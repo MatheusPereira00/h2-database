e353b6538c51f3202ef9a7e58262301f863f1592
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-06 17:40:28 -0300

    init

--- src/main/java/com/example/coursedatabase/CoursedatabaseApplication.java
+++ src/main/java/com/example/coursedatabase/CoursedatabaseApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoursedatabaseApplication {
 
+    // class inicio
     public static void main(String[] args) {
         SpringApplication.run(CoursedatabaseApplication.class, args);
     }
