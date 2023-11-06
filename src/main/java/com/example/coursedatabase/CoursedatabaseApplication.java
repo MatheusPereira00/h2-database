7714e0dd6ed25ed664617aa9ce78b41357a1c184
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-06 17:36:08 -0300

    first

---
+++ src/main/java/com/example/coursedatabase/CoursedatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursedatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursedatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursedatabase/CoursedatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursedatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
