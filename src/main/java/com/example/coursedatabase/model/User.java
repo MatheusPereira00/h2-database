d10db786c47f45513b8b7155ef636faa6171683f
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-07 10:16:35 -0300

    feat: add h2-database

--- src/main/java/com/example/coursedatabase/model/User.java
+++ src/main/java/com/example/coursedatabase/model/User.java
@@ -1,5 +1,9 @@
 package com.example.coursedatabase.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +12,13 @@ import org.springframework.data.annotation.Id;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @jakarta.persistence.Id
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private  String email;
